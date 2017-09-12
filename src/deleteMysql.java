11cf9468586887d690b2190d7fcdf92190553e3f
cyzAdmin <891354032@qq.com>
2017-09-12 18:54:41 +0800

    mysql增删改查

--- src/deleteMysql.java
+++ src/deleteMysql.java
@@ -1,12 +1,11 @@
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class selectMysql {
+public class deleteMysql {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -33,11 +32,29 @@ public class selectMysql {
 			while (rs.next()) {
 				System.out.print(rs.getInt(1) + "\t");
 				System.out.print(rs.getString(2) + "\t");
-				System.out.print(rs.getString(3) + "\t");
+				System.out.println(rs.getString(3) + "\t");
 			}
-			    rs.close();
-	            stmt.close();
-	            conn.close();
+			
+			//删除数据
+			String sql3="delete from db_database01.tb_user where id=?";
+			PreparedStatement pst = conn.prepareStatement(sql3);
+			pst = conn.prepareStatement(sql3);
+			pst.setInt(1,1);
+			pst.executeUpdate();
+			
+			String sql4 = "select *from db_database01.tb_user";
+			ResultSet rs2 = stmt.executeQuery(sql4);
+
+			System.out.println("id" + "\t" + "账号" + "\t" + "密码");
+			while (rs2.next()) {
+				System.out.print(rs2.getInt(1) + "\t");
+				System.out.print(rs2.getString(2) + "\t");
+				System.out.println(rs2.getString(3) + "\t");
+			}
+			
+			rs.close();
+			stmt.close();
+			conn.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
--- src/updateMysql.java
+++ src/updateMysql.java
@@ -1,12 +1,11 @@
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class selectMysql {
+public class updateMysql {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -27,17 +26,26 @@ public class selectMysql {
 			conn = DriverManager.getConnection(url, username, password);
 			Statement stmt = conn.createStatement();// 创建Statement对象
 			System.out.println("成功链接到数据库");
+
+			//修改数据库
+			String sql2="update db_database01.tb_user set username=? where id=?";
+			PreparedStatement pst = conn.prepareStatement(sql2);
+			pst.setString(1,"chenyzh");//第一个参数
+            pst.setInt(2,1);//第二个参数
+            pst.executeUpdate();
+ 
 			String sql = "select *from db_database01.tb_user";
 			ResultSet rs = stmt.executeQuery(sql);// 创建数据对象
 			System.out.println("id" + "\t" + "账号" + "\t" + "密码");
 			while (rs.next()) {
 				System.out.print(rs.getInt(1) + "\t");
 				System.out.print(rs.getString(2) + "\t");
-				System.out.print(rs.getString(3) + "\t");
+				System.out.println(rs.getString(3) + "\t");
 			}
-			    rs.close();
-	            stmt.close();
-	            conn.close();
+			
+			rs.close();
+			stmt.close();
+			conn.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
